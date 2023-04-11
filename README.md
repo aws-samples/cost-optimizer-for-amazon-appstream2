2d20480378a495c411c6883d713617cf0cba7494
Dylan Barlett <barlettd@amazon.com>
2023-04-11 11:27:07 -0400

    Support configurable interval between notification. Closes #1.

--- README.md
+++ README.md
@@ -18,8 +18,10 @@ This AWS CloudFormation template deploys the Cost Optimizer for Amazon AppStream
 
    | Parameter | Default | Description |
    | --- | --- | --- |
-   | **Image builder notification threshold** | `2` | How long (in hours) an image builder can run before notifications are sent. Set to `0` to disable notifications. |
+   | **Image builder active notification threshold** | `2` | How long (in hours) an image builder can run before notifications are sent. Set to `0` to disable notifications. |
+   | **Image builder active notification interval** | `1` | How long (in hours) between image builder active notifications. Minimum: `1`. |
    | **Image builder stop threshold** | `12` | How long (in hours) an image builder can run before being stopped. Set to `0` to allow image builders to run indefinitely. |
+   | **Image builder stop notifications** | `Yes` | Possible values: `No` and `Yes`. |
    | **Email address** | `<Requires input>` | Email address to receive notifications. |
    | **SNS topic customer master key (CMK)** | `alias/aws/sns` | For more information about SNS topic encryption, see [Encryption at rest](https://docs.aws.amazon.com/sns/latest/dg/sns-server-side-encryption.html). |
 
@@ -28,7 +30,7 @@ This AWS CloudFormation template deploys the Cost Optimizer for Amazon AppStream
    | Parameter | Default  | Description |
    | --- | --- | --- |
    | **Python logging level** | `INFO` | Possible values are: `CRITICAL`, `ERROR`, `WARNING`, `INFO`, and `DEBUG`. |
-   | **Log retention period** | `7` | Days to retain function logs. Possible values are: `1`, `3`, `5`, `7`, `14`, `30`, `60`, `90`, `120`, `150`, `180`, `365`, `400`, `545`, `731`, `1827`, `2192`, `2557`, `2922`, `3288`, and `3653`. |
+   | **Log retention period** | `7` | Days to retain function logs. Possible values: `1`, `3`, `5`, `7`, `14`, `30`, `60`, `90`, `120`, `150`, `180`, `365`, `400`, `545`, `731`, `1827`, `2192`, `2557`, `2922`, `3288`, and `3653`. |
    | **Timeout** | `60` | The amount of time (in seconds) that Lambda allows the function to run before stopping it. Minimum: `1`, maximum: `900`. |
    | **Architecture** | `x86_64` | `x86_64` is supported in all AWS Regions. `arm64` costs less, but is not supported in all AWS Regions. See [Lambda instruction set architectures](https://docs.aws.amazon.com/lambda/latest/dg/foundation-arch.html) for more information. |
 
@@ -61,18 +63,19 @@ Tags: {
 }
 ```
 
+Each image builder will trigger a maximum of one active notification per specified interval (the `Image builder active notification interval` parameter).
 If the image builder has been active for longer than the `Image builder stop threshold` parameter (default: 12 hours), the notification subject is `Stopping image builder <name>`.
 
-Each image builder will trigger a maximum of one notification per hour.
-
 ## Opt out image builders
-To opt out of notifications for an image builder, apply a resource tag to the image builder using the tag key `Skip_Active_Notification` and any tag value.
-To opt out of stopping an image builder, apply a resource tag to the image builder using the tag key `Skip_Stop` and any tag value.
+To opt out an image builder from active notifications, being stopped, and/or stop notifications, apply one or more of the following resource tags to the image builder, with any tag value:
+* `Skip_Active_Notification`
+* `Skip_Stop`
+* `Skip_Stop_Notification`
 
 ## Costs
 You are responsible for the cost of the AWS services used while running this solution.
 The total cost of running this solution depends on the number of image builders in your account and the number of notifications sent.
-As of March 2023, the cost for running this solution with default settings in the US East (N. Virginia) Region is approximately $0.31 per month for an account with 10 image builders and 100 notifications per month.
+As of April 2023, the cost for running this solution with default settings in the US East (N. Virginia) Region is approximately $0.31 per month for an account with 10 image builders and 100 notifications per month.
 
 ## License
 This solution is licensed under the MIT-0 License. See the LICENSE file.
--- source/lambda/image_builder_monitor.py
+++ source/lambda/image_builder_monitor.py
@@ -15,29 +15,51 @@ from typing import Dict, List, Optional, Tuple
 import boto3
 from botocore.exceptions import ClientError
 
-IB_TABLE_NAME: str = os.environ["IB_TABLE_NAME"]
+logger: logging.Logger = logging.getLogger()
+LOG_LEVEL: str = str(os.environ["LOG_LEVEL"])
+logger.setLevel(LOG_LEVEL)
+
 IB_ACTIVE_STATES: Tuple = (
     "PENDING",
     "UPDATING_AGENT",
     "RUNNING",
     "REBOOTING"
 )
+IB_TABLE_NAME: str = os.environ["IB_TABLE_NAME"]
+logging.debug("IB_TABLE_NAME: %s", IB_TABLE_NAME)
+
 IB_NOTIFY_HOURS: float = float(os.environ["IB_NOTIFY_HOURS"])
-IB_STOP_HOURS: float = float(os.environ["IB_STOP_HOURS"])
-SNS_TOPIC_ARN: str = os.environ["SNS_TOPIC_ARN"]
-
-logger: logging.Logger = logging.getLogger()
-LOG_LEVEL: str = str(os.environ["LOG_LEVEL"])
-logger.setLevel(LOG_LEVEL)
-
 if IB_NOTIFY_HOURS > 0:
-    logging.info("IB_NOTIFY_HOURS: %f", IB_NOTIFY_HOURS)
+    logging.debug("IB_NOTIFY_HOURS: %f", IB_NOTIFY_HOURS)
 else:
-    logging.info("IB_NOTIFY_HOURS: %f (disabled)", IB_NOTIFY_HOURS)
+    logging.debug("IB_NOTIFY_HOURS: %f (disabled)", IB_NOTIFY_HOURS)
+
+IB_NOTIFY_INTERVAL_HOURS: float = float(os.environ["IB_NOTIFY_INTERVAL_HOURS"])
+logging.debug("IB_NOTIFY_INTERVAL_HOURS: %f", IB_NOTIFY_INTERVAL_HOURS)
+IB_NOTIFY_INTERVAL_SEC: float = IB_NOTIFY_INTERVAL_HOURS * 3600
+
+IB_STOP_HOURS: float = float(os.environ["IB_STOP_HOURS"])
 if IB_STOP_HOURS > 0:
-    logging.info("IB_STOP_HOURS: %f", IB_STOP_HOURS)
+    logging.debug("IB_STOP_HOURS: %f", IB_STOP_HOURS)
 else:
-    logging.info("IB_STOP_HOURS: %f (disabled)", IB_STOP_HOURS)
+    logging.debug("IB_STOP_HOURS: %f (disabled)", IB_STOP_HOURS)
+
+IB_STOP_NOTIFY: bool
+if os.environ["IB_STOP_NOTIFY"] == "No":
+    IB_STOP_NOTIFY = False
+    logging.debug("IB_STOP_NOTIFY: False (disabled)")
+elif os.environ["IB_STOP_NOTIFY"] == "Yes":
+    IB_STOP_NOTIFY = True
+    logging.debug("IB_STOP_NOTIFY: True (enabled)")
+else:
+    IB_STOP_NOTIFY = True
+    logging.warning(
+        "IB_STOP_NOTIFY: unsupported value (%s), defaulting to True (enabled)",
+        IB_STOP_NOTIFY
+    )
+
+SNS_TOPIC_ARN: str = os.environ["SNS_TOPIC_ARN"]
+logging.debug("SNS_TOPIC_ARN: %s", SNS_TOPIC_ARN)
 
 # Regional AppStream 2.0 clients are created later.
 session: boto3.session.Session = boto3.Session()
@@ -57,7 +79,7 @@ def get_supported_as2_regions() -> List[str]:
 def publish_image_builder_notification(
     notification_type: str,
     image_builder: Dict,
-    active_time: str,
+    active_duration: str,
     tags: Dict,
 ):
     """Publish an SNS notification about an image builder.
@@ -65,12 +87,14 @@ def publish_image_builder_notification(
     subject: str
     if notification_type == "active":
         subject = (
-            f"Image builder {image_builder['Name']} active for {active_time}"
+            f"Image builder {image_builder['Name']} active for {active_duration}"
         )
     elif notification_type == "stop":
         subject = (
             f"Stopping image builder {image_builder['Name']}"
         )
+    else:
+        logging.error("Unknown notification type: %s", notification_type)
 
     arn_split = image_builder["Arn"].split(":")
 
@@ -82,11 +106,181 @@ def publish_image_builder_notification(
             f"Region: {arn_split[3]}",
             f"Name: {image_builder['Name']}",
             f"Instance type: {image_builder['InstanceType']}",
-            f"Time active: {active_time}",
+            f"Time active: {active_duration}",
             f"Tags: {json.dumps(tags, indent=2)}"
         ))
     )
     return response
+
+
+def process_previously_active_image_builder(
+    image_builder: Dict,
+    ddb_item: Dict,
+    response_dt: datetime,
+    expiration_date: int,
+    as2
+):
+    """
+    The image builder is stopped if all the following are true:
+    * IB_STOP_HOURS > 0 (not disabled globally).
+    * active_hours > IB_STOP_HOURS.
+    * Skip_Stop tag is not present.
+
+    A stop notification is sent if all the following are true:
+    * The image builder will be stopped.
+    * IB_STOP_NOTIFY is True (not disabled globally).
+    * Skip_Stop_Notification tag is not present.
+
+    An active notification is sent if all the following are true:
+    * The image builder will not be stopped.
+    * IB_NOTIFY_HOURS > 0 (not disabled globally).
+    * active_hours > IB_NOTIFY_HOURS.
+    * At least IB_NOTIFY_INTERVAL_HOURS has elapsed since the last notification.
+    * Skip_Active_Notification tag is not present.
+    """
+    active_hours: float = (
+        response_dt - datetime.fromisoformat(ddb_item["earliest_active"])
+    ).total_seconds() / 3600
+    logging.info(
+        "%s: active for %f hour(s)",
+        image_builder["Name"],
+        active_hours
+    )
+
+    now: datetime = datetime.now()
+    stop: bool = False
+    notify_stop: bool = False
+    notify_active: bool = False
+
+    # Step 1: determine tentative stop actions.
+    if active_hours > IB_STOP_HOURS > 0:
+        stop = True  # Unless tagged (checked later)
+    if stop and IB_STOP_NOTIFY:
+        notify_stop = True  # Unless tagged (checked later)
+
+    # Step 2: determine tentative active notification.
+    if active_hours > IB_NOTIFY_HOURS > 0:
+        since_last_notification: timedelta = (
+            now - datetime.fromisoformat(
+                ddb_item["last_active_notification"]
+            )
+        )
+        if since_last_notification.total_seconds() < IB_NOTIFY_INTERVAL_SEC:
+            logging.info(
+                "%s: less than %f hour(s) since last notification",
+                image_builder["Name"],
+                IB_NOTIFY_INTERVAL_HOURS
+            )
+        else:
+            notify_active = True
+
+    # Step 3: retrieve tags and override actions accordingly.
+    if stop or notify_stop or notify_active:
+        tags = as2.list_tags_for_resource(
+            ResourceArn=image_builder["Arn"]
+        )["Tags"]
+        if "Skip_Stop" in tags:
+            stop = False
+            notify_stop = False
+            logging.info(
+                "%s: Skip_Stop tag present",
+                image_builder["Name"]
+            )
+        if "Skip_Stop_Notification" in tags:
+            notify_stop = False
+            logging.info(
+                "%s: Skip_Stop_Notification tag present",
+                image_builder["Name"]
+            )
+        if "Skip_Active_Notification" in tags:
+            notify_active = False
+            logging.info(
+                "%s: Skip_Active_Notification tag present",
+                image_builder["Name"]
+            )
+
+    # Step 4: take actions.
+    if notify_stop or notify_active:
+        # Format active hours for notification.
+        active_duration: str
+        if int(active_hours) == 1:
+            active_duration = "1 hour"
+        else:
+            active_duration = f"{int(active_hours)} hours"
+
+        notification_type: str = "active"
+        if notify_stop:
+            notification_type = "stop"
+
+        logger.info(
+            "%s: sending %s notification",
+            image_builder["Name"],
+            notification_type
+        )
+        response = publish_image_builder_notification(
+            notification_type=notification_type,
+            image_builder=image_builder,
+            active_duration=active_duration,
+            tags=tags
+        )
+        logger.debug(response)
+        if notification_type == "active":
+            table.update_item(
+                Key={
+                    "region": as2.meta.region_name,
+                    "name": image_builder["Name"]
+                },
+                UpdateExpression=(
+                    "set last_active_notification=:l,"
+                    "exp_date=:e"
+                ),
+                ExpressionAttributeValues={
+                    ":l": now.isoformat(),
+                    ":e": expiration_date,
+                }
+            )
+    else:
+        # Take no action except extending TTL.
+        table.update_item(
+            Key={
+                "region": as2.meta.region_name,
+                "name": image_builder["Name"]
+            },
+            UpdateExpression="set exp_date=:e",
+            ExpressionAttributeValues={
+                ":e": expiration_date,
+            },
+        )
+    if stop:
+        logger.info(
+            "%s: stopping",
+            image_builder["Name"]
+        )
+        response = as2.stop_image_builder(
+            Name=image_builder["Name"]
+        )
+        logger.debug(response)
+
+
+def process_newly_active_image_builder(
+    region: str,
+    name: str,
+    earliest_active: datetime,
+    expiration_date: int
+):
+    """Process a newly-active (not previously seen) image builder.
+    """
+    logging.info(
+        "%s: newly active",
+        name
+    )
+    table.put_item(Item={
+        "region": region,
+        "name": name,
+        "earliest_active": earliest_active.isoformat(),
+        "last_active_notification": datetime.min.isoformat(),
+        "exp_date": expiration_date,
+    })
 
 
 def process_active_image_builder(
@@ -95,22 +289,11 @@ def process_active_image_builder(
     response_dt: datetime
 ):
     """Process an active image builder.
-
-    The image builder is stopped if all the following are true:
-    * IB_STOP_HOURS > 0 (not disabled globally).
-    * active_hours > IB_STOP_HOURS.
-    * Skip_Stop tag is not present (checked later).
-
-    A notification is sent if all the following are true:
-    * IB_NOTIFY_HOURS > 0 (not disabled globally).
-    * active_hours > IB_NOTIFY_HOURS.
-    * At least one hour has elapsed since the last notification.
-    * Skip_Active_Notification tag is not present.
     """
     # Calculate expiration date (TTL) one day in the future. This allows
     # DynamoDB to delete items for image builders that transition from active to
     # deleted between invocations.
-    expiration_date_int: int = int((
+    expiration_date: int = int((
         response_dt + timedelta(days=1)
     ).timestamp())
 
@@ -119,105 +302,20 @@ def process_active_image_builder(
         "name": image_builder["Name"]
     })
     if "Item" in ddb_response:
-        # Image builder has been seen before, so calculate active hours.
-        item: Dict = ddb_response["Item"]
-        active_hours: float = (
-            response_dt - datetime.fromisoformat(item["earliest_active"])
-        ).total_seconds() / 3600
-        logging.info(
-            "Image builder %s has been active for %f hour(s)",
-            image_builder["Name"],
-            active_hours
+        process_previously_active_image_builder(
+            image_builder=image_builder,
+            ddb_item=ddb_response["Item"],
+            response_dt=response_dt,
+            expiration_date=expiration_date,
+            as2=as2
         )
-
-        stop_unless_tagged: bool = False
-        if active_hours > IB_STOP_HOURS > 0:
-            stop_unless_tagged = True
-
-        notify_unless_tagged: bool = False
-        if active_hours > IB_NOTIFY_HOURS > 0:
-            now: datetime = datetime.now()
-            since_last_notification: timedelta = (
-                now - datetime.fromisoformat(item["last_notification"])
-            )
-            if since_last_notification.total_seconds() < 3600:
-                logging.info("Less than one hour since last notification")
-            else:
-                notify_unless_tagged = True
-
-        if stop_unless_tagged or notify_unless_tagged:
-            tags = as2.list_tags_for_resource(
-                ResourceArn=image_builder["Arn"]
-            )["Tags"]
-
-            # Format active hours for notification.
-            active_duration: str
-            if int(active_hours) == 1:
-                active_duration = "1 hour"
-            else:
-                active_duration = f"{int(active_hours)} hours"
-
-            notification_type: str = "active"
-            if stop_unless_tagged:
-                if "Skip_Stop" in tags:
-                    logger.info("Skip_Stop tag present")
-                else:
-                    notification_type = "stop"
-                    logger.info("Stopping image builder")
-                    response = as2.stop_image_builder(
-                        Name=image_builder["Name"]
-                    )
-                    logger.debug(response)
-
-            if notify_unless_tagged:
-                if "Skip_Active_Notification" in tags:
-                    logger.info("Skip_Active_Notification tag present")
-                else:
-                    logger.info("Sending %s notification", notification_type)
-                    response = publish_image_builder_notification(
-                        notification_type=notification_type,
-                        image_builder=image_builder,
-                        active_time=active_duration,
-                        tags=tags
-                    )
-                    logger.debug(response)
-                    table.update_item(
-                        Key={
-                            "region": as2.meta.region_name,
-                            "name": image_builder["Name"]
-                        },
-                        UpdateExpression=(
-                            "set last_notification=:l,"
-                            "exp_date=:e"
-                        ),
-                        ExpressionAttributeValues={
-                            ":l": now.isoformat(),
-                            ":e": expiration_date_int,
-                        },
-                    )
-        else:
-            # Take no action except updating TTL.
-            table.update_item(
-                Key={
-                    "region": as2.meta.region_name,
-                    "name": image_builder["Name"]
-                },
-                UpdateExpression="set exp_date=:e",
-                ExpressionAttributeValues={
-                    ":e": expiration_date_int,
-                },
-            )
     else:
-        logging.info(
-            "Image builder %s is newly active", image_builder["Name"]
+        process_newly_active_image_builder(
+            region=as2.meta.region_name,
+            name=image_builder["Name"],
+            earliest_active=response_dt,
+            expiration_date=expiration_date
         )
-        table.put_item(Item={
-            "region": as2.meta.region_name,
-            "name": image_builder["Name"],
-            "earliest_active": response_dt.isoformat(),
-            "last_notification": datetime.min.isoformat(),
-            "exp_date": expiration_date_int,
-        })
 
 
 def process_image_builders(region: str):
@@ -241,8 +339,8 @@ def process_image_builders(region: str):
             image_builders.extend(response.get("ImageBuilders", []))
             next_token = response.get("NextToken", None)
         logging.info("Found %i image builder(s)", len(image_builders))
-    except ClientError as e:
-        logging.error(e)
+    except ClientError as err:
+        logging.error(err)
 
     for builder in image_builders:
         if builder["State"] in IB_ACTIVE_STATES:
@@ -252,7 +350,7 @@ def process_image_builders(region: str):
                 response_dt=ib_response_dt
             )
         else:
-            logging.info("Image builder %s is inactive", builder["Name"])
+            logging.info("%s: inactive", builder["Name"])
             table.delete_item(Key={
                 "region": region,
                 "name": builder["Name"]
@@ -263,6 +361,5 @@ def process_image_builders(region: str):
 def lambda_handler(event: Dict, context: Dict):
     """Lambda handler.
     """
-    regions: List[str] = get_supported_as2_regions()
-    for region in regions:
+    for region in get_supported_as2_regions():
         process_image_builders(region=region)
